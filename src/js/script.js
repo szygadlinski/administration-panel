e66d3b20a84c8ad6f7ee56a3f530e53cf9b8ff5c
Szymon Zygadliński <szygadlinski@gmail.com>
2021-01-10 18:22:15 +0100

    Add RWD for sidebar & topbar, add js for collapsing them

---
+++ src/js/script.js
@@ -0,0 +1,48 @@
+{
+  'use strict';
+
+  const select = {
+    collapsingButton: '.collapsing-button',
+    collapsingElements: '.collapsing',
+    extendingElements: '.extending',
+  };
+
+  const classNames = {
+    collapsed: 'collapsed',
+    extended: 'extended',
+  };
+
+  class Buttons {
+
+    constructor(){
+      const thisButtons = this;
+
+      thisButtons.getElements();
+      thisButtons.initButtons();
+    }
+
+    getElements(){
+      const thisButtons = this;
+
+      thisButtons.dom = {};
+      thisButtons.dom.collapsingButton = document.querySelector(select.collapsingButton);
+      thisButtons.dom.collapsingElements = document.querySelectorAll(select.collapsingElements);
+      thisButtons.dom.extendingElements = document.querySelectorAll(select.extendingElements);
+    }
+
+    initButtons(){
+      const thisButtons = this;
+
+      thisButtons.dom.collapsingButton.addEventListener('click', function(){
+        for (const collapsingElement of thisButtons.dom.collapsingElements){
+          collapsingElement.classList.toggle(classNames.collapsed);
+        }
+        for (const extendingElement of thisButtons.dom.extendingElements){
+          extendingElement.classList.toggle(classNames.extended);
+        }
+      });
+    }
+  }
+
+  new Buttons();
+}
